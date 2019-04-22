1b5fee3e71499eae0dee6056cce80977f0b8b26d
drifter-s <48032010+drifter-s@users.noreply.github.com>
2019-04-21 23:55:52 -0500

    Update CustomerServiceImpl.java

--- src/com/democrm/springdemo/service/CustomerServiceImpl.java
+++ src/com/democrm/springdemo/service/CustomerServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.luv2code.springdemo.dao.CustomerDAO;
+import com.democrm.springdemo.dao.CustomerDAO;
 import com.democrm.springdemo.entity.Customer;
 
 @Service
